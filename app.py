5ea89c727a0a5b1ffdbe19c9adf74c076b684278
Nirbhay Shegale <shegalenirbhay@gmail.com>
2026-04-11 05:14:55 +0530

    Update API URL to production endpoint

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import plotly.express as px
 import pandas as pd
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://customer-churn-prediction-retention-nwue.onrender.com"
 
 
 # --- Custom CSS for styling ---
